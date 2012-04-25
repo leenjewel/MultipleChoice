f7e44941ac6369200f5cc8a534245a5886091498
leenjewel <leenjewel@leenjewel-VBOX>
2012-04-25 18:38:36 +0800

    调整滚轮滑动速度

--- src/org/leenjewel/multiplechoice/panel/JScrollPaneWithComponent.java
+++ src/org/leenjewel/multiplechoice/panel/JScrollPaneWithComponent.java
@@ -12,7 +12,7 @@ public class JScrollPaneWithComponent extends JScrollPane {
 		super(c, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 		this.component = c;
 		this.setPreferredSize(this.component.getPreferredSize());
-                this.getVerticalScrollBar().setUnitIncrement(this.component.getPreferredSize().height/40);
+                this.getVerticalScrollBar().setUnitIncrement(this.component.getPreferredSize().height/100);
 	}
 
 	public Component getComponent(){
